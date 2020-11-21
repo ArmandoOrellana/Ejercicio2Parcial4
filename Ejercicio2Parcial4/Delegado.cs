714c2d6b1411e465d5e32f21b161cda6bfcfc199
ArmandoOrellana <73487533+ArmandoOrellana@users.noreply.github.com>
2020-11-20 21:14:37 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio2Parcial4/Delegado.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial4Ejercicio2
+{
+
+    delegate double Pago(double a);
+
+    class Delegado
+    {
+
+        public double Pago(double x)
+        {
+            if (x <= 10000)
+            {
+
+                return x;
+            }
+            else if (x <= 20000)
+            {
+
+                return x - (x * 0.1);
+            }
+            else if (x <= 50000)
+            {
+                return x - (x * 0.3);
+            }
+            else
+            {
+                return x - (x * 0.5);
+            }
+        }
+    }
+}
---
+++ Ejercicio2Parcial4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial4Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Delegado dlg = new Parcial4Ejercicio2.Delegado();
+            Console.WriteLine("Resultado");
+            Console.WriteLine("Digite la cantidad el monto a comprar");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Total a pagar es de:");
+            Console.WriteLine(dlg.Pago(a));
+            Console.ReadKey();
+
+        }
+    }
+}
